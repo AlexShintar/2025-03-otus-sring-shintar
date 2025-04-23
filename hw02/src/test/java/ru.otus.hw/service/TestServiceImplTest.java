a5fae09bfb1fff535bf6a14bdd8c057b272f1037
Ash <alex@shintar.ru>
2025-04-23 15:51:05 +0300

    Migrate implementations from first assignment into template and add annotation-based injection

---
+++ hw02/src/test/java/ru.otus.hw/service/TestServiceImplTest.java
@@ -0,0 +1,34 @@
+package ru.otus.hw.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class TestServiceImplTest {
+
+    @Mock
+    private IOService ioService;
+
+    @Mock
+    private QuestionDao questionDao;
+
+    @InjectMocks
+    private TestServiceImpl testService;
+
+}
