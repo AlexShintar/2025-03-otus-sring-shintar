708c4e24e636d0e1bb9f4ee6fcc316270e29a457
Ash <alex@shintar.ru>
2025-04-28 19:07:04 +0300

    Switch to Spring-boot starters, use of localization-aware methods, application banner added

--- hw03/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw03/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,12 +1,21 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Component
@@ -15,11 +24,28 @@ public class CsvQuestionDao implements QuestionDao {
 
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+        String fileName = fileNameProvider.getTestFileName();
+        if (fileName == null || fileName.trim().isEmpty()) {
+            throw new QuestionReadException("File name is not provided");
+        }
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream resourceStream = classLoader.getResourceAsStream(fileName);
+        if (resourceStream == null) {
+            throw new QuestionReadException("Resource not found: " + fileName);
+        }
+        try (Reader reader = new InputStreamReader(resourceStream, StandardCharsets.UTF_8)) {
+            CsvToBean<QuestionDto> csvToBean = new CsvToBeanBuilder<QuestionDto>(reader)
+                    .withType(QuestionDto.class)
+                    .withSkipLines(1)
+                    .withSeparator(';')
+                    .build();
 
-        return new ArrayList<>();
+            return csvToBean.parse()
+                    .stream()
+                    .map(QuestionDto::toDomainObject)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new QuestionReadException("Error reading CSV file", e);
+        }
     }
 }
--- hw03/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw03/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -3,8 +3,13 @@ package ru.otus.hw.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,13 +26,36 @@ public class TestServiceImpl implements TestService {
         ioService.printLine("");
 
         var questions = questionDao.findAll();
+        if (questions.isEmpty()) {
+            throw new QuestionReadException(ioService.getMessage("TestService.error.noQuestions"));
+        }
         var testResult = new TestResult(student);
+        int questionIndex = 1;
 
-        for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
-            testResult.applyAnswer(question, isAnswerValid);
+        for (var question : questions) {
+            askAndEvaluateQuestion(question, testResult, questionIndex++);
         }
         return testResult;
     }
 
+    private void askAndEvaluateQuestion(Question question, TestResult result, int index) {
+        printQuestionWithAnswers(question, index);
+
+        int max = question.answers().size();
+        String prompt = ioService.getMessage("TestService.prompt.select");
+        String errorMessage = ioService.getMessage("TestService.error.select", 1, max);
+
+        int choice = ioService.readIntForRangeWithPrompt(1, max, prompt, errorMessage);
+
+        boolean isAnswerValid = question.answers().get(choice - 1).isCorrect();
+        result.applyAnswer(question, isAnswerValid);
+    }
+
+    private void printQuestionWithAnswers(Question question, int index) {
+        ioService.printFormattedLineLocalized("TestService.prompt.question", index, question.text());
+        List<Answer> answers = question.answers();
+        for (int i = 0; i < answers.size(); i++) {
+            ioService.printFormattedLine("  %d) %s", i + 1, answers.get(i).text());
+        }
+    }
 }
