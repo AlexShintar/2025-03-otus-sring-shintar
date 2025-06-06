bfed9359b6691bc46778b49bb692619b36ea40d0
Ash <alex@shintar.ru>
2025-06-06 13:18:31 +0300

    hw07: Add @NonNull to repository methods

--- hw07/src/main/java/ru/otus/hw/repositories/CommentRepository.java
+++ hw07/src/main/java/ru/otus/hw/repositories/CommentRepository.java
@@ -2,14 +2,17 @@ package ru.otus.hw.repositories;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import ru.otus.hw.models.Comment;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    @NonNull
     @EntityGraph(value = "comments-books-entity-graph")
     List<Comment> findAll();
 
+    @NonNull
     @EntityGraph(value = "comments-books-entity-graph")
     List<Comment> findAllByBookId(long bookId);
 }
